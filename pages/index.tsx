0acbff33b3eabfe1ae45a4ed4d9153ed1f8c1279
Joseph Chow <contact@josephchow.dev>
2023-02-10 14:21:51 -0500

    :wrench: added site metadata :wrench:

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import {TipProvider} from '@/src/context/tipContext';
 import Attributions from '@/src/layout/Attributions/Attributions';
 import TipCalculator from '@/src/layout/TipCalculator/TipCalculator';
 import Title from '@/src/layout/Title/Title';
+import Meta from '@/src/Meta';
 import styles from '@/styles/Home.module.css';
 import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
   return (
     <div className={styles['app']}>
+      <Meta />
       <Title />
       <TipProvider>
         <TipCalculator />
