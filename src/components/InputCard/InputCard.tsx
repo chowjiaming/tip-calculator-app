94de03fb43af802869b23f5b6e8647a5e57f73ea
Joseph Chow <contact@josephchow.dev>
2023-04-21 20:03:55 -0400

    :construction: refactored input card :recycle:

--- src/components/InputCard/InputCard.tsx
+++ src/components/InputCard/InputCard.tsx
@@ -3,90 +3,97 @@ import {useTipContext} from '../../utils/tipCalculatorContext';
 import {TipPercentageBoxes} from '../TipPercentageBoxes/TipPercentageBoxes';
 import './InputCard.css';
 
+/**
+ * InputCard component displays input fields for bill amount and number of people
+ * and includes the TipPercentageBoxes component for selecting the tip percentage.
+ *
+ * @returns {JSX.Element} The rendered InputCard component.
+ */
 export function InputCard(): JSX.Element {
   const {state, dispatch} = useTipContext();
 
-  const handleBillChange = (
-    e: JSX.TargetedEvent<HTMLInputElement, Event>
+  /**
+   * Handles changes in bill amount and number of people input fields.
+   *
+   * @param {JSX.TargetedEvent<HTMLInputElement, Event>} e - The input event object.
+   * @param {'UPDATE_BILL' | 'UPDATE_PEOPLE'} actionType - The action type to be dispatched.
+   */
+  const handleChange = (
+    e: JSX.TargetedEvent<HTMLInputElement, Event>,
+    actionType: 'UPDATE_BILL' | 'UPDATE_PEOPLE'
   ): void => {
     dispatch({
-      type: 'UPDATE_BILL',
-      // strange preact bug, need to cast to HTMLInputElement
+      type: actionType,
       payload: Number((e.target as HTMLInputElement).value),
     });
   };
-  const handlePeopleChange = (
-    e: JSX.TargetedEvent<HTMLInputElement, Event>
-  ): void => {
-    dispatch({
-      type: 'UPDATE_PEOPLE',
-      // strange preact bug, need to cast to HTMLInputElement
-      payload: Number((e.target as HTMLInputElement).value),
-    });
-  };
+
+  /**
+   * Handles input blur by dispatching the 'INPUT_BLUR' action.
+   */
   const handleInputBlur = (): void => {
     dispatch({type: 'INPUT_BLUR'});
   };
 
+  /**
+   * Renders an input field for either bill amount or number of people.
+   *
+   * @param {string} title - The title of the input field.
+   * @param {string} iconName - The name of the icon for the input field.
+   * @param {'UPDATE_BILL' | 'UPDATE_PEOPLE'} actionType - The action type to be dispatched on input change.
+   * @param {number | null} stateValue - The current value of the input field from the state.
+   * @param {string} stateError - The current error message from the state.
+   * @returns {JSX.Element} The rendered input field component.
+   */
+  const renderInputField = (
+    title: string,
+    iconName: string,
+    actionType: 'UPDATE_BILL' | 'UPDATE_PEOPLE',
+    stateValue: number | null,
+    stateError: string
+  ): JSX.Element => (
+    <>
+      <h2 class={`input__header ${stateError ? 'input__header--error' : ''}`}>
+        {title}
+      </h2>
+      <div class="input__wrapper">
+        {stateError ? (
+          <span class="input__message input__message--error">{stateError}</span>
+        ) : null}
+        <img
+          src={`/svg/icon-${iconName}.svg`}
+          alt={`icon-${iconName}`}
+          class={`input__icon ${stateError ? 'input__icon--error' : ''}`}
+        />
+        <input
+          type="text"
+          class={`input__input ${stateError ? 'input__input--error' : ''}`}
+          placeholder={'0'}
+          value={stateValue || ''}
+          onInput={(e) => handleChange(e, actionType)}
+          onBlur={handleInputBlur}
+        />
+      </div>
+    </>
+  );
+
   return (
     <div class="input">
-      <h2
-        class={`input__header ${state.billError ? 'input__header--error' : ''}`}
-      >
-        Bill
-      </h2>
-      <div class="input__wrapper">
-        {state.billError ? (
-          <span class="input__message input__message--error">
-            {state.billError}
-          </span>
-        ) : null}
-        <img
-          src="/svg/icon-dollar.svg"
-          alt="icon-dollar"
-          class={`input__icon ${state.billError ? 'input__icon--error' : ''}`}
-        />
-        <input
-          type="text"
-          class={`input__input ${state.billError ? 'input__input--error' : ''}`}
-          placeholder={'0'}
-          value={state.billAmount || ''}
-          onInput={handleBillChange}
-          onBlur={handleInputBlur}
-        />
-      </div>
+      {renderInputField(
+        'Bill',
+        'dollar',
+        'UPDATE_BILL',
+        state.billAmount,
+        state.billError
+      )}
       <TipPercentageBoxes />
-      <h2
-        class={`input__header ${
-          state.numberOfPeopleError ? 'input__header--error' : ''
-        }`}
-      >
-        Number of People
-      </h2>
-      <div class="input__wrapper">
-        {state.numberOfPeopleError ? (
-          <span class="input__message input__message--error">
-            {state.numberOfPeopleError}
-          </span>
-        ) : null}
-        <img
-          src="/svg/icon-person.svg"
-          alt="icon-person"
-          class={`input__icon ${
-            state.numberOfPeopleError ? 'input__icon--error' : ''
-          }`}
-        />
-        <input
-          type="text"
-          class={`input__input ${
-            state.numberOfPeopleError ? 'input__input--error' : ''
-          }`}
-          placeholder={'0'}
-          value={state.numberOfPeople || ''}
-          onInput={handlePeopleChange}
-          onBlur={handleInputBlur}
-        />
-      </div>
+      {renderInputField(
+        'Number of People',
+        'person',
+        'UPDATE_PEOPLE',
+        state.numberOfPeople,
+        state.numberOfPeopleError
+      )}
     </div>
   );
 }
