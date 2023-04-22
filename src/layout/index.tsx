a277e67bac9ce4ed7ac4a908dff4c81289fd9a4e
Joseph Chow <contact@josephchow.dev>
2023-04-21 23:46:40 -0400

    :technologist: added comments to layout component and typescript parser :memo:

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,17 +1,25 @@
 import type {JSX} from 'preact/jsx-runtime';
 
+// Define the Props type for the Layout component.
 type Props = {
   children: JSX.Element;
 };
 
+// The Layout component takes children as props and wraps them with the
+// application's header, content, and footer.
 export function Layout({children}: Props): JSX.Element {
   return (
     <div class="app">
+      {/* Header section with the title */}
       <header class="title">
         <h1 class="title__text">spli</h1>
         <h1 class="title__text">tter</h1>
       </header>
+
+      {/* The main content, passed as children */}
       {children}
+
+      {/* Footer section with attribution */}
       <footer class="attribution">
         <div class="attribution__text">
           Design inspired by{' '}
